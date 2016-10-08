b10358f9db45cd995a31581e1c528172cc37dcb7
Annie Zhang <annie7hang@gmail.com>
2016-10-08 18:26:00 -0400

    Two test cases added

--- tester.py
+++ tester.py
@@ -1,5 +1,4 @@
 from crawler import crawler as c
-import server
 import unittest
 
 
@@ -17,8 +16,20 @@ class CrawlerTest(unittest.TestCase):
             'u', 'v', 'w', 'x', 'y', 'z', 'and', 'or',
     ])
 
+    URL1 = 'http://localhost:8080/test/1/testcode1.html'
+    URL2 = 'http://localhost:8080/test/1/testcode2.html'
+    URL3 = 'http://localhost:8080/test/1/testcode3.html'
+
+    TEST1_1 = '''testing code 1 the cat in the hat to
+                        testcode2 to testcode3 '''
+    TEST1_2 = '''testing code 2 the quick fox jumped over to testcode4 '''
+    TEST1_3 = '''testing code 3 the lazy brown
+                 dog to testcode6 to testcode7 '''
+
     def setUp(self):
-        server.main()
+        print '!!!Make sure to have server.py running at localhost:8080!!!'
+        print 'Check the server requests to ensure things running correctly!'
+        print "(If not, restart server)"
 
     def test1(self):
         ''' Checks that words are correctly found
@@ -28,8 +39,32 @@ class CrawlerTest(unittest.TestCase):
         inv_index = bot.get_resolved_inverted_index()
         words = set(inv_index.keys())
 
-        ground_truth = '''testing code 1 the cat in the hat to
-                        testcode2 to testcode3'''
+        ground_truth = CrawlerTest.test1_1
+        ground_truth = set(ground_truth.split()) - CrawlerTest.ignored_words
+        self.assertTrue(words == ground_truth)
+
+    def test2(self):
+        ''' Checks the crawler goes across pages and keeps correct
+            document to word mappings
+        '''
+        bot = c(None, 'test1.txt')
+        bot.crawl(depth=1)
+        inv_index = bot.get_resolved_inverted_index()
+        words = set(inv_index.keys())
+
+        secnd_page_words = CrawlerTest.test1_2
+
+        # Check that all words in doc2 references doc2
+        # and that nothing else does.
+        for word in inv_index:
+            if word in secnd_page_words:
+                self.assertTrue(CrawlerTest.URL2 in inv_index[word])
+            else:
+                self.assertTrue(CrawlerTest.URL2 not in inv_index[word])
+
+        # Compare the lists of found words to ensure
+        # that first 3 pages have been visited (and only the first three)
+        ground_truth = CrawlerTest.TEST1_1 + CrawlerTest.TEST1_2 + CrawlerTest.TEST1_3
         ground_truth = set(ground_truth.split()) - CrawlerTest.ignored_words
         self.assertTrue(words == ground_truth)
 
